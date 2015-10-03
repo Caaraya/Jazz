c16556ce96689c944c990b07db598e3c33cd84f3
Caaraya <StevePhillipsCa@gmail.com>
2015-10-03 12:49:59 -0700

    added completion feature to jazz sourceview widget

--- jazz_sourceview.cpp
+++ jazz_sourceview.cpp
@@ -1,5 +1,7 @@
 #include "jazz_sourceview.hpp"
+#include <gtksourceview/gtksource.h>
 #include <gtksourceview/gtksourcebuffer.h>
+
 namespace Jazz
 {
 	SourceView::SourceView():
@@ -7,8 +9,19 @@ namespace Jazz
 	{	
 		add(*Glib::wrap(GTK_WIDGET(source_view)));
 		
+		completion = gtk_source_view_get_completion(source_view);
+		completion_words = gtk_source_completion_words_new("Suggestions", nullptr);
+		gtk_source_completion_words_register(completion_words, gtk_text_view_get_buffer(GTK_TEXT_VIEW(source_view)));
+		gtk_source_completion_add_provider(completion,
+			GTK_SOURCE_COMPLETION_PROVIDER(completion_words), nullptr);
+		
 		show_all();
 	}
+	SourceView::~SourceView()
+	{
+		g_object_unref(completion_words);
+	}
+	
 	GtkSourceBuffer* SourceView::get_buffer() const
 	{
 		return GTK_SOURCE_BUFFER(gtk_text_view_get_buffer(GTK_TEXT_VIEW(source_view)));
--- jazz_sourceview.hpp
+++ jazz_sourceview.hpp
@@ -1,14 +1,20 @@
 #include <gtksourceview/gtksourceview.h>
 #include <gtkmm.h>
+#include <gtksourceview/completion-providers/words/gtksourcecompletionwords.h>
 namespace Jazz
 {
 	class SourceView : public Gtk::ScrolledWindow
 	{
 	public:
 		SourceView();
+		~SourceView();
 		GtkSourceView* get_sourceview() const;
 		GtkSourceBuffer* get_buffer() const;
 	private:
 		GtkSourceView* source_view = nullptr;
+		GtkSourceCompletion* completion = nullptr;
+		GtkSourceCompletionWords* completion_words = nullptr;
+	private:
+		void UserAction();
 	};
 }
