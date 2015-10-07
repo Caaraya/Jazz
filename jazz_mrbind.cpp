c789ef8da30b5954245f39d01fb32118978ee6f2
Caaraya <StevePhillipsCa@gmail.com>
2015-10-06 18:08:22 -0700

    replaced mrubybind submodule with one that is more up to date

--- jazz_mrbind.cpp
+++ jazz_mrbind.cpp
@@ -6,7 +6,10 @@ namespace Jazz
 	Gtk::Window* new_window() { return new Gtk::Window(); }
 	void JazzIDE::MrbBind()
 	{
-		mrubybind::MrubyBind binder(mrb);
+		RClass* mod = mrb_define_module(mrb, "Gtk");
+		
+		mrubybind::MrubyBind binder(mrb, mod);
+		
 		binder.bind_class("Window", new_window);
 		binder.bind_instance_method("Window", "show_all", &Gtk::Window::show_all);
 		binder.bind_instance_method("Window", "set_default_size", &Gtk::Window::set_default_size);
